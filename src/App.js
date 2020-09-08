73d55a8693a94fde5223e520a7e8b8761d46de8f
ampsteric <ampsteric@gmail.com>
2020-09-08 17:02:45 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+    return (
+        <div className="App">
+            <img src="https://api.memegen.link/images/doge/memes/memes_everywhere.png" />
+        </div>
+    );
+}
+
+export default App;
